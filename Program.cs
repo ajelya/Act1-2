e418ceed916bf0d706f5e7ade8fb491341f19002
Maria Angela Lorena <mariaangelamlorena@iskolarngbayan.pup.edu.ph>
2025-09-18 00:26:41 +0800

    Console Complimentzz

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Console of Compliments!<3\n");
+
+Console.Write("Please enter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("\nHow are you feeling today, " + name + "?");
+
+Console.Write("(1)Happy\n(2)Sad\nMOOD= ");
+int choice = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+if (choice == 1)
+{
+    Console.WriteLine("<3 Happiness looks good on you!");
+}
+else if (choice == 2)
+{
+    Console.WriteLine("</3 Everything will work out just fine! Keep going!");
+}
+else
+{
+    Console.WriteLine("Invalid choice! Please try again:)");
+}
